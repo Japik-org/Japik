d057d662806369e697b8c21105615073985ef0ef
pro100kryto <pro100kryto.original@gmail.com>
2021-11-01 05:07:12 -0300

    SettingListenerContainer: String defaultValue -> Object defaultValueToString Allow passing Object instead of String

--- src/main/java/com/pro100kryto/server/settings/SettingListenerContainer.java
+++ src/main/java/com/pro100kryto/server/settings/SettingListenerContainer.java
@@ -1,15 +1,18 @@
 package com.pro100kryto.server.settings;
 
+import lombok.Getter;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Objects;
 
 
+@Getter
 public final class SettingListenerContainer {
     private final String key;
     private final ISettingListener listener;
     private final SettingListenerEventMask eventMask;
     private final boolean isOptional;
+    @Nullable
     private final String defaultValue;
 
     /**
@@ -36,15 +39,15 @@ public final class SettingListenerContainer {
     /**
      * get value from settings or default with default mask
      */
-    public SettingListenerContainer(String key, ISettingListener listener, String defaultValue) {
-        this(key, listener, SettingListenerEventMask.DEFAULT, true, defaultValue);
+    public SettingListenerContainer(String key, ISettingListener listener, Object defaultValueToString) {
+        this(key, listener, SettingListenerEventMask.DEFAULT, true, defaultValueToString);
     }
 
     /**
      * get value from settings or default with custom mask
      */
-    public SettingListenerContainer(String key, ISettingListener listener, SettingListenerEventMask eventMask, String defaultValue) {
-        this(key, listener, eventMask, true, defaultValue);
+    public SettingListenerContainer(String key, ISettingListener listener, SettingListenerEventMask eventMask, Object defaultValueToString) {
+        this(key, listener, eventMask, true, defaultValueToString);
     }
 
     /**
@@ -53,38 +56,18 @@ public final class SettingListenerContainer {
      * @param listener - setting listener
      * @param eventMask - listen a specific events
      * @param isOptional - true if is not obligatory to have set a value
-     * @param defaultValue - use default value for this setting if is optional
+     * @param defaultValueToString - use default value for this setting if is optional
      */
     public SettingListenerContainer(String key,
                                     ISettingListener listener,
                                     SettingListenerEventMask eventMask,
                                     boolean isOptional,
-                                    String defaultValue) {
+                                    @Nullable
+                                    Object defaultValueToString) {
         this.key = Objects.requireNonNull(key);
         this.listener = Objects.requireNonNull(listener);
         this.eventMask = Objects.requireNonNull(eventMask);
         this.isOptional = isOptional;
-        this.defaultValue = defaultValue;
-    }
-
-    public String getKey() {
-        return key;
-    }
-
-    public ISettingListener getListener() {
-        return listener;
-    }
-
-    public SettingListenerEventMask getEventMask() {
-        return eventMask;
-    }
-
-    public boolean isOptional() {
-        return isOptional;
-    }
-
-    @Nullable
-    public String getDefaultValue() {
-        return defaultValue;
+        this.defaultValue = (defaultValueToString!=null ? defaultValueToString.toString() : null);
     }
 }
